65f12edbb82c8ff1a256a320b5438c7b6b529af2
Fabian Beuke <mail@beuke.org>
2018-06-02 16:01:49 +0200

    Hask: refactor Main.hs

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -8,30 +8,22 @@ import Data.Maybe (fromMaybe)
 {- Git commands -}
 
 successOrNothing :: (ExitCode, a, b) -> Maybe a
-successOrNothing (exitCode, output, _) =
-	if exitCode == ExitSuccess then Just output else Nothing
+successOrNothing (exitCode, output, _)
+     | exitCode == ExitSuccess = Just output
+     | otherwise = Nothing
 
 safeRun :: String -> [String] -> IO (Maybe String)
-safeRun command arguments =
-	do -- IO
-		output <- readProcessWithExitCode command arguments ""
-		return (successOrNothing output)
+safeRun cmd args =
+        successOrNothing <$> readProcessWithExitCode cmd args mempty
 
 gitrevparse :: IO (Maybe Hash)
-gitrevparse = do -- IO
-		mresult <- safeRun "git" ["rev-parse", "--short", "HEAD"]
-		let rev = do -- Maybe
-			result <- mresult
-			return (MkHash (init result))
-		return rev
-
-{- main -}
+gitrevparse = do
+        mresult <- safeRun "git" ["rev-parse", "--short", "HEAD"]
+        return $ MkHash . init <$> mresult
 
 main :: IO ()
-main = do -- IO
-	status <- getContents
-	mhash <- unsafeInterleaveIO gitrevparse -- defer the execution until we know we need the hash
-	let result = do -- Maybe
-		strings <- stringsFromStatus mhash status
-		return (unwords strings)
-	putStr (fromMaybe "" result)
+main = do
+    status <- getContents
+    -- defer the execution until we know we need the hash
+    mhash <- unsafeInterleaveIO gitrevparse
+    putStr . fromMaybe mempty $ unwords <$> stringsFromStatus mhash status
