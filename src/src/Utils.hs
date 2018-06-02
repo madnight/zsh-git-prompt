598ca03f88ac51371f7f80fee7eaf3766ed74c50
Fabian Beuke <mail@beuke.org>
2018-06-02 16:53:08 +0200

    Hask: refactor Utils.hs

--- src/src/Utils.hs
+++ src/src/Utils.hs
@@ -1,6 +1,5 @@
 module Utils where
 
-import Data.Maybe (fromMaybe)
 import BranchParse (Branch(MkBranch), MBranchInfo, BranchInfo(MkBranchInfo), branchInfo, getDistance, pairFromDistance, Remote)
 import StatusParse (Status(MakeStatus), processStatus)
 
@@ -20,50 +19,42 @@ processBranch = rightOrNothing . branchInfo
 
 processGitStatus :: [String] -> Maybe GitInfo
 processGitStatus [] = Nothing
-processGitStatus (branchLine:statusLines) =
-		do -- Maybe
-			mbranch <- processBranch branchLine
-			status <- processStatus statusLines
-			return (MkGitInfo mbranch status)
+processGitStatus (branchLine:statusLines) = do
+            mbranch <- processBranch branchLine
+            status <- processStatus statusLines
+            return (MkGitInfo mbranch status)
 
 showStatusNumbers :: Status Int -> [String]
-showStatusNumbers (MakeStatus s x c t) =
-		do -- List
-			nb <- [s, x, c, t]
-			return (show nb)
+showStatusNumbers (MakeStatus s x c t) = show <$> [s, x, c, t]
 
 showRemoteNumbers :: Maybe Remote -> [String]
-showRemoteNumbers mremote =
-		do -- List
-			ab <- [ahead, behind]
-			return (show ab)
-	where
-		(ahead, behind) = fromMaybe (0,0) distance  -- the script needs some value, (0,0) means no display
-		distance = do -- Maybe
-			remote <- mremote
-			dist <- getDistance remote
-			return (pairFromDistance dist)
+showRemoteNumbers mremote = show <$> [ahead, behind]
+    where -- the script needs some value, (0,0) means no display
+        (ahead, behind) =
+            maybe (0, 0) pairFromDistance (getDistance =<< mremote)
+
 
 showBranchInfo :: BranchInfo -> [String]
-showBranchInfo (MkBranchInfo branch mremote) = show branch : showRemoteNumbers mremote
+showBranchInfo (MkBranchInfo branch mremote) =
+    show branch : showRemoteNumbers mremote
 
 {- Combine status info, branch info and hash -}
 
 branchOrHashWith :: Char -> Maybe Hash -> Maybe BranchInfo -> BranchInfo
 branchOrHashWith _ _ (Just bi) = bi
-branchOrHashWith c (Just hash) Nothing = MkBranchInfo (MkBranch (c : getHash hash)) Nothing
+branchOrHashWith c (Just hash) Nothing =
+    MkBranchInfo (MkBranch (c : getHash hash)) Nothing
 branchOrHashWith _ Nothing _ = MkBranchInfo (MkBranch "") Nothing
 
 showGitInfo :: Maybe Hash
-			-> GitInfo
-			-> [String]
-showGitInfo mhash (MkGitInfo bi stat) = branchInfoString ++ showStatusNumbers stat
-	where
-		branchInfoString = showBranchInfo (branchOrHashWith ':' mhash bi)
+            -> GitInfo
+            -> [String]
+showGitInfo mhash (MkGitInfo bi stat) =
+    branchInfoString ++ showStatusNumbers stat
+    where
+        branchInfoString = showBranchInfo (branchOrHashWith ':' mhash bi)
 
 stringsFromStatus :: Maybe Hash
-					-> String -- status
-					-> Maybe [String]
-stringsFromStatus h status = do -- List
-		processed <- processGitStatus (lines status)
-		return (showGitInfo h processed)
+                    -> String -- status
+                    -> Maybe [String]
+stringsFromStatus hash = (showGitInfo hash <$>) . processGitStatus . lines
