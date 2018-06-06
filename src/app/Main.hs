5223494764179fc7d283ed895abdc40a8b3ddf04
Fabian Beuke <mail@beuke.org>
2018-06-06 21:49:00 +0200

    Hask: add getStashCount function in haskell

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -3,11 +3,16 @@ import System.Exit (ExitCode(ExitSuccess))
 import System.IO.Unsafe (unsafeInterleaveIO)
 
 import Utils (stringsFromStatus, Hash(MkHash))
-import Data.Maybe (fromMaybe)
 
-import Data.Git (findRepo)
+import Filesystem.Path.Rules (encodeString, posix)
+import Data.Git.Storage (findRepoMaybe)
+import Data.Git.Named
+import Data.Git.Ref (HashAlgorithm, SHA1)
 import System.Posix.IO (stdInput)
 import System.Posix.Terminal (queryTerminal)
+import Data.Maybe (fromMaybe)
+
+import qualified Control.Exception.Base as E
 
 {- Git commands -}
 
@@ -25,18 +30,34 @@ gitrevparse = do
         mresult <- safeRun "git" ["rev-parse", "--short", "HEAD"]
         return $ MkHash . init <$> mresult
 
+getStashCount :: String -> IO Int
+getStashCount repo = countLinesInFile $ repo ++ "/logs/refs/stash"
+    where
+        countLinesInFile :: String -> IO Int
+        countLinesInFile f = length . lines <$> E.catch (readFile f) readHandler
+        readHandler :: IOError -> IO String
+        readHandler _ = pure mempty
+
+{- isMergeInProgess :: IO Bool -}
+{- isMergeInProgess = do -}
+{-     repo <- findRepoMaybe -}
+
+parse status = do
+    repo <- findRepoMaybe
+    stashCount <- getStashCount . fromMaybe mempty $ encodeString posix <$> repo
+    mhash <- unsafeInterleaveIO gitrevparse
+    let parseStatus =  maybe mempty unwords . stringsFromStatus mhash
+    let echo x = " " ++ show x
+    putStr $ parseStatus status
+    putStr $ echo stashCount
+
 main :: IO ()
 main = do
-    repo <- findRepo
     isTTY <- queryTerminal stdInput
-    mhash <- unsafeInterleaveIO gitrevparse
-    let parse = putStr .  maybe mempty unwords . stringsFromStatus mhash
     if isTTY
         then do -- run status command
             status <- safeRun "git" ["status", "--porcelain", "--branch"]
             case status of
                 Nothing -> error "fatal: not a git repository"
                 Just s -> parse s
-        else do -- get status from stdin
-            status <- getContents
-            parse status
+        else getContents >>= parse -- get status from stdin
